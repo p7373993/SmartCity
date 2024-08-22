09869d37befea49b2c0cf2c17ef889178910e174
rhkrgusqls <79958305+rhkrgusqls@users.noreply.github.com>
2024-08-22 10:59:30 +0900

    Commentary

--- Source/ProtoType/Global/Properties.h
+++ Source/ProtoType/Global/Properties.h
@@ -1,5 +1,11 @@
-#pragma once
+﻿#pragma once
 #include <string>
 
 #define PORT 4960
 #define ServerIP "203.249.91.175"
+
+/*$ git update - index --assume - unchanged Properties.h
+Properties(서버 IP나 PORT 등등 개인 설정값) 변경 무시하기(개인설정으로 프로젝트 시험할때 사용)
+
+git update - index --no - assume - unchanged properties.h
+Properties(서버 IP나 PORT 등등 개인 설정값) 변경 무시 해제하기(프로퍼티에 자원 추가하고싶을때 사용)*/
