5cea37fc140b67a5a7388d4efe20595f33ed5c14
sungwoo <p7373993@gmail.com>
2024-08-22 17:55:00 +0900

    마우스 움직임 ㄷ구현

---
+++ Source/ProtoType/Private/Character/SMCharacterMoveComponent.cpp
@@ -0,0 +1,160 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/SMCharacterMoveComponent.h"
+#include "GameFramework/Character.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputMappingContext.h"
+#include "InputAction.h"
+#include "GameFramework/PlayerController.h"
+#include "Camera/CameraComponent.h"
+
+// Sets default values for this component's properties
+USMCharacterMoveComponent::USMCharacterMoveComponent()
+{
+	OwningActor = Cast<ACharacter>(GetOwner());
+
+	static ConstructorHelpers::FObjectFinder<UInputMappingContext> MappingContextRef(TEXT("/Script/EnhancedInput.InputMappingContext'/Game/Sungwoo/Input/IMC_Default.IMC_Default'"));
+	if (MappingContextRef.Object)
+	{
+		InputMapping = MappingContextRef.Object;
+	}
+
+	static ConstructorHelpers::FObjectFinder<UInputAction> MoveActionRef(TEXT("/Script/EnhancedInput.InputAction'/Game/Sungwoo/Input/IA_Move.IA_Move'"));
+	if (MoveActionRef.Object)
+	{
+		MoveAction = MoveActionRef.Object;
+	}
+
+	static ConstructorHelpers::FObjectFinder<UInputAction> LeftClickActionRef(TEXT("/Script/EnhancedInput.InputAction'/Game/Sungwoo/Input/IA_LeftClick.IA_LeftClick'"));
+	if (LeftClickActionRef.Object)
+	{
+		LeftClickAction = LeftClickActionRef.Object;
+	}
+
+	static ConstructorHelpers::FObjectFinder<UInputAction> RightClickActionRef(TEXT("/Script/EnhancedInput.InputAction'/Game/Sungwoo/Input/IA_RightClick.IA_RightClick'"));
+	if (RightClickActionRef.Object)
+	{
+		RightClickAction = RightClickActionRef.Object;
+	}
+
+	static ConstructorHelpers::FObjectFinder<UInputAction> UpDownActionRef(TEXT("/Script/EnhancedInput.InputAction'/Game/Sungwoo/Input/IA_UpDown.IA_UpDown'"));
+	if (UpDownActionRef.Object)
+	{
+		UpDownAction = UpDownActionRef.Object;
+	}
+
+	bIsLeftClicking = false;
+	bIsRightClicking = false;
+
+
+
+}
+
+
+// Called when the game starts
+void USMCharacterMoveComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	APlayerController* PlayerController = Cast<APlayerController>(OwningActor->GetInstigatorController());
+	if (PlayerController)
+	{
+		// Enhanced Input Subsystem을 가져옴
+		UEnhancedInputLocalPlayerSubsystem* InputSubsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer());
+
+		// 인풋 매핑 컨텍스트 추가
+		if (InputSubsystem && InputMapping)
+		{
+			InputSubsystem->AddMappingContext(InputMapping, 0);
+		}
+
+		// 인풋 액션 바인딩
+		if (UEnhancedInputComponent* Input = Cast<UEnhancedInputComponent>(PlayerController->InputComponent))
+		{
+			// 좌클릭 액션 바인딩
+			Input->BindAction(LeftClickAction, ETriggerEvent::Started, this, &USMCharacterMoveComponent::OnLeftClick);
+			Input->BindAction(LeftClickAction, ETriggerEvent::Completed, this, &USMCharacterMoveComponent::OnLeftClick);
+
+			// 우클릭 액션 바인딩
+			Input->BindAction(RightClickAction, ETriggerEvent::Started, this, &USMCharacterMoveComponent::OnRightClick);
+			Input->BindAction(RightClickAction, ETriggerEvent::Completed, this, &USMCharacterMoveComponent::OnRightClick);
+
+			// 이동 액션 바인딩
+			Input->BindAction(MoveAction, ETriggerEvent::Triggered, this, &USMCharacterMoveComponent::QuaterMove);
+
+			//드래그 액션
+			Input->BindAction(UpDownAction, ETriggerEvent::Triggered, this, &USMCharacterMoveComponent::UpDown);
+		}
+	}
+}
+
+void USMCharacterMoveComponent::QuaterMove(const FInputActionValue& Value)
+{
+	FVector2D MovementVector = Value.Get<FVector2D>();
+
+	if (bIsLeftClicking)
+	{
+		UCameraComponent* Camera = OwningActor->FindComponentByClass<UCameraComponent>();
+		ensure(Camera);
+		// 좌클릭 상태에서의 이동 처리
+		if (Camera)
+		{
+
+			float Height = FMath::Clamp(OwningActor->GetActorLocation().Z * 0.005, 0, 500);
+
+			FVector CameraForward = Camera->GetForwardVector();
+			FVector CameraRight = Camera->GetRightVector();
+
+			FVector MoveDirection = CameraForward * MovementVector.Y + CameraRight * MovementVector.X;
+			MoveDirection.Z = 0.0f;
+
+			FVector CurrentLocation = OwningActor->GetActorLocation();
+			FVector NewLocation = CurrentLocation + (MoveDirection * Height);
+
+			OwningActor->SetActorLocation(NewLocation, true);
+
+
+		}
+	}
+
+	if (bIsRightClicking)
+	{
+		// 우클릭 상태에서의 화면 회전 처리
+		APlayerController* PlayerController = Cast<APlayerController>(OwningActor->Controller);
+		if (PlayerController)
+		{
+			FRotator NewRotation = PlayerController->GetControlRotation();
+			NewRotation.Yaw += MovementVector.X;
+			NewRotation.Pitch = FMath::Clamp(NewRotation.Pitch + MovementVector.Y, -89.0f, 0.0f);
+			PlayerController->SetControlRotation(NewRotation);
+
+			FString DebugMessage = FString::Printf(TEXT("Look with Right Click (%f:%f)"), MovementVector.X, MovementVector.Y);
+		}
+	}
+}
+
+//좌클릭
+void USMCharacterMoveComponent::OnLeftClick(const FInputActionValue& Value)
+{
+	bIsLeftClicking = Value.Get<bool>();
+}
+//우클릭
+void USMCharacterMoveComponent::OnRightClick(const FInputActionValue& Value)
+{
+	bIsRightClicking = Value.Get<bool>();
+}
+//휠로 위아래
+void USMCharacterMoveComponent::UpDown(const FInputActionValue& Value)
+{
+	float Height = FMath::Clamp(OwningActor->GetActorLocation().Z * 0.1, 0, 1000);
+	float Power = Value.Get<float>();
+	FVector CurrentLocation = OwningActor->GetActorLocation();
+	FVector NewLocation = CurrentLocation + FVector(0.0f, 0.0f, Power * Height); // 이동 속도를 조정합니다.
+
+	OwningActor->SetActorLocation(NewLocation, true); // true는 충돌을 고려하여 위치를 설정합니다.
+}
+
+
+
---
+++ Source/ProtoType/Public/Character/SMCharacter.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "SMCharacter.generated.h"
+
+UCLASS()
+class PROTOTYPE_API ASMCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ASMCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+
+//기본 캐릭터 조작
+protected:
+
+	UPROPERTY(VisibleAnywhere, meta = (AllowPrivateAcess = "true"))
+	class UCameraComponent* Camera;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Move, Meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<class USMCharacterMoveComponent> DefaultMove;
+};
